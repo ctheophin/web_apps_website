1fcc5f5440583915be3eca13cd3b3d1c5a161eed
Courtney Theophin <ctheophin6@gmail.com>
2018-11-09 15:20:49 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(() => {
+$('figure').hide();
+
+$('#lion-thumb').on('click',() => {
+$('figure').hide();
+  $('#lion').show();
+
+});
+$('#menthumb').on('click',() => {
+$('figure').hide();
+  $('#men').show();
+
+});
+$('#landthumb').on('click',() => {
+$('figure').hide();
+  $('#land').show();
+});
+$('#momthumb').on('click',() => {
+$('figure').hide();
+  $('#mom').show();
+
+});
+
+
+
+
+
+
+
+});
